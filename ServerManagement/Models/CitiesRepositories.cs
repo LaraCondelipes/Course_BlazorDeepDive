e2d5262b583aa31b10418b1ec3cc954516846c7d
LaraCondelipes <LaraCondelipes07@gmail.com>
2024-08-02 11:57:24 +0100

    Add project files.

---
+++ ServerManagement/Models/CitiesRepositories.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerManagement.Models
+{
+    public class CitiesRepositories
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        //the function GetCities() just returns the list of cities
+        public static List<string> GetCities() => cities;
+
+    }
+}
---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+        //represents whether the server is online or offline
+        public int ServerId { get; set; }
+        public  bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        //city where the server is located
+        public string? City { get; set; }
+    }
+}
